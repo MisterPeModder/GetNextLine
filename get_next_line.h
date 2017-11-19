9e133f29734e370bae0df85e40639206a3d305fe
Yanis GUAYE <yguaye44@gmail.com>
2017-11-19 14:35:50 +0100

    wip refactoring

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: yguaye <yguaye@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/17 12:59:21 by yguaye            #+#    #+#             */
-/*   Updated: 2017/11/17 17:32:46 by yguaye           ###   ########.fr       */
+/*   Updated: 2017/11/19 14:14:48 by yguaye           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,12 @@
 # define GET_NEXT_LINE_H
 
 # define BUFF_SIZE 9999
+
+typedef struct		s_buffer
+{
+	char[BUFF_SIZE]	buff;
+	int				index;
+}					t_buffer;
 
 int		get_next_line(const int fd, char **line);
 
