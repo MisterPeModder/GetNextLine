9d9ae2ad24040b762940fc64b6af55b9f58fa15a
Yanis GUAYE <yguaye@student.42.fr>
2017-12-02 16:53:32 +0100

    MOAR Fixes!

--- get_next_line.c
+++ get_next_line.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yguaye <yguaye44@gmail.com>                +#+  +:+       +#+        */
+/*   By: yguaye <yguaye@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/11/19 13:35:48 by yguaye            #+#    #+#             */
-/*   Updated: 2017/11/29 16:05:49 by yguaye           ###   ########.fr       */
+/*   Created: 2017/11/30 14:46:00 by yguaye            #+#    #+#             */
+/*   Updated: 2017/12/01 17:16:55 by yguaye           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,104 +16,127 @@
 #include "libft.h"
 #include <stdio.h>
 
-static t_buff	*gnl_buff(t_buff **beg, int fd)
+static t_buff    *gnl_buff(t_buff **beg, int fd)
 {
-	t_buff	*buff;
-
-	if (beg && *beg)
-	{
-		buff = *beg;
-		while (buff)
-		{
-			if (buff->fd == fd)
-				return (buff);
-			buff = buff->next;
-		}
-	}
-	if (!(buff = (t_buff *)malloc(sizeof(t_buff))))
-		return (NULL);
-	buff->fd = fd;
-	buff->i = -1;
-	buff->next = NULL;
-	if (beg && *beg)
-	{
-		buff->next = *beg;
-		*beg = buff;
-	}
-	else
-		*beg = buff;
-	return (buff);
+    t_buff    *buff;
+    
+    if (beg && *beg)
+    {
+        buff = *beg;
+        while (buff)
+        {
+            if (buff->fd == fd)
+                return (buff);
+            buff = buff->next;
+        }
+    }
+    if (!(buff = (t_buff *)malloc(sizeof(t_buff))))
+        return (NULL);
+    buff->fd = fd;
+    buff->i = -1;
+    buff->lsiz=0;
+    buff->next = NULL;
+    if (beg && *beg)
+    {
+        buff->next = *beg;
+        *beg = buff;
+    }
+    else
+        *beg = buff;
+    return (buff);
 }
 
-static void		gnl_reset_buff(t_buff *buff)
+static int        gnl_reset_or_read(t_buff *buff, int *ret, int fd, int sread)
 {
-	while (buff->i > 0 && --buff->i >= 0)
-		buff->val[buff->i] = 0;
-	buff->i = -1;
+    if (sread)
+    {
+        if(buff->i == -1 || buff->i == 0)
+        {
+            if ((*ret = read(fd, buff->val, BUFF_SIZE)) <= 0)
+                return (1);
+            buff->lsiz = *ret;
+        }
+    }
+    else if (!REACHED_END(buff))
+    {
+        while (buff->i > 0 && --buff->i >= 0)
+            buff->val[buff->i] = 0;
+        buff->i = -1;
+    }
+    return (REACHED_END(buff));
 }
 
-static int		gnl_free_buff(int ret, t_buff **beg, t_buff *buff)
+static int        gnl_free_buff(int ret, t_buff **beg, t_buff *buff)
 {
-	t_buff	*b;
-
-	if (*beg == buff)
-		ft_memdel((void **)beg);
-	else
-	{
-		b = *beg;
-		while (b->next && b->next != buff)
-			b = b->next;
-		if (b->next == buff)
-		{
-			b->next = buff->next;
-			free(buff);
-		}
-	}
-	return (ret);
+    t_buff    *b;
+    
+    ret = REACHED_END(buff) ? 1 : ret;
+    if (*beg == buff)
+        ft_memdel((void **)beg);
+    else
+    {
+        b = *beg;
+        while (b->next && b->next != buff)
+            b = b->next;
+        if (b->next == buff)
+        {
+            b->next = buff->next;
+            free(buff);
+        }
+    }
+    return (ret);
 }
 
-static void		gnl_join(t_buff *buff, char **line, int j)
+static void        gnl_join(t_buff *buff, char **line, int j)
 {
-	char	*nstr;
-	char	*tmp;
-
-	nstr = ft_strsub(buff->val, j, buff->i - j);
-	if (*line)
-	{
-		tmp = ft_strjoin(*line, nstr);
-		free(*line);
-		*line = tmp;
-		free(nstr);
-	}
-	else
-		*line = nstr;
+    char    *nstr;
+    char    *tmp;
+    
+    if (!(buff->lsiz == 1 && buff->val[0] == '\n'))
+    {
+        nstr = ft_strsub(buff->val, j, buff->i - j);
+        if (*line)
+        {
+            tmp = ft_strjoin(*line, nstr);
+            free(*line);
+            *line = tmp;
+            free(nstr);
+        }
+        else
+            *line = nstr;
+    }
+    else
+    {
+        if (!*line)
+            *line = (char *)ft_memalloc(1);
+        gnl_reset_or_read(buff, 0, 0, 0);
+        //++buff->i;
+    }
 }
 
-int				get_next_line(const int fd, char **line)
+int                get_next_line(const int fd, char **line)
 {
-	static t_buff	*beg;
-	t_buff			*buff;
-	int				ret;
-	int				j;
-
-	if (!(buff = gnl_buff(&beg, fd)))
-		return (-1);
-	ret = 1;
-	*line = NULL;
-	while (buff->i != BUFF_SIZE)
-	{
-		if (buff->i == -1 || buff->i == 0)
-			if ((ret = read(fd, buff->val, BUFF_SIZE)) <= 0)
-				break ;
-		j = ++buff->i;
-		while (buff->i < BUFF_SIZE && buff->val[buff->i] != '\n')
-			++buff->i;
-		gnl_join(buff, line, j);
-		printf("i: %d, j: %d", buff->i, j);
-		if (buff->i != BUFF_SIZE)
-			return (1);
-		else if (ret != 0)
-			gnl_reset_buff(buff);
-	}
-	return (gnl_free_buff(ret, &beg, buff));
+    static t_buff    *beg;
+    t_buff            *buff;
+    int                ret;
+    int                j;
+    
+    if (!(buff = gnl_buff(&beg, fd)))
+        return (-1);
+    ret = 1;
+    *line = NULL;
+    while (buff->i != BUFF_SIZE)
+    {
+        if (gnl_reset_or_read(buff, &ret, fd, 1))
+            break ;
+        j = ++buff->i;
+        while (buff->i < buff->lsiz && buff->val[buff->i] != '\n')
+            ++buff->i;
+        gnl_join(buff, line, j);
+        if ((buff->lsiz == 1 && buff->val[0] == '\n') || buff->i != buff->lsiz)
+            return (1);
+        else if (ret != 0)
+            gnl_reset_or_read(buff, 0, 0, 0);
+    }
+    return (gnl_free_buff(ret, &beg, buff));
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,20 +6,22 @@
 /*   By: yguaye <yguaye@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/17 12:59:21 by yguaye            #+#    #+#             */
-/*   Updated: 2017/11/29 14:37:16 by yguaye           ###   ########.fr       */
+/*   Updated: 2017/11/30 11:59:06 by yguaye           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 42
+# define BUFF_SIZE 465
+# define REACHED_END(buff) (buff->i == buff->lsiz && buff->i < BUFF_SIZE)
 
 typedef struct		s_buff
 {
 	char			val[BUFF_SIZE];
 	int				i;
 	int				fd;
+	int				lsiz;
 	struct s_buff	*next;
 }					t_buff;
 
